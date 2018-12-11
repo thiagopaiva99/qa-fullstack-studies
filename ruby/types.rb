4a26514c6e231aba306edad6ee1d44138f017ff7
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-12-11 17:16:42 -0200

    Creating a file to test the types in Ruby

---
+++ ruby/types.rb
@@ -0,0 +1,18 @@
+# types in ruby are dinamically
+
+product = 'Mackbook Pro'
+price = 5999.99
+amount = 10
+available = true
+
+# print the variable type
+puts product.class
+puts price.class
+puts amount.class
+puts available.class
+
+# print the variable value
+puts product
+puts price
+puts amount
+puts available
