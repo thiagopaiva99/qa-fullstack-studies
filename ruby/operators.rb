b994f945e937370a5f4e1be552fba2d363503f85
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-12-11 17:24:28 -0200

    Creating a file to test the ruby operators

---
+++ ruby/operators.rb
@@ -0,0 +1,17 @@
+number1 = 0
+number2 = 0
+total = 0
+
+puts 'Please, write the first value: '
+number1 = gets.chomp.to_i
+
+puts 'Please, write the second value: '
+number2 = gets.chomp.to_i
+
+total = number1 + number2
+# total = number1 - number2
+# total = number1 * number2
+# total = number1 / number2
+
+puts 'The value total of the sum is: '
+puts total
