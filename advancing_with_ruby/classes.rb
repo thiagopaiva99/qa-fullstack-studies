3a699d6a508fbac427b65a7e7e1473c5b83a1cbf
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-12-14 14:55:30 -0200

    Working with classes constructors

--- advancing_with_ruby/classes.rb
+++ advancing_with_ruby/classes.rb
@@ -1,13 +1,20 @@
 class Account
   attr_accessor :balance
+  attr_accessor :name
+
+  def initialize(name)
+    self.balance = 0.0
+    self.name = name
+  end
 
   def deposit(value)
     self.balance += value
-    puts "Depoisiting the amount of R$ #{value}"
+
+    puts "Depoisiting the amount of R$ #{value} at #{self.name} account, the total balance is R$ #{self.balance}"
   end
 end
 
-account = Account.new
+account = Account.new('Thiago')
 
-account.balance = 0.0
 account.deposit(50)
+account.deposit(20)
