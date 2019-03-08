9d47dc489b2fbadbb5e5c3790abebe872bbb74f7
Thiago Galvani <thiagopaiva99@gmail.com>
2019-03-08 20:46:31 -0300

    Refactoring the test file and moving the class implementation for a correct folder

--- unit_tests/spec/withdraw_spec.rb
+++ unit_tests/spec/withdraw_spec.rb
@@ -1,23 +1,4 @@
-class CheckingAccount
-    attr_accessor :balance
-    attr_accessor :message
-    attr_accessor :limitForDay
-
-    def initialize(balance)
-        self.balance = balance
-        self.limitForDay = 700
-    end
-
-    def withdraw(amount)
-        if (amount > self.balance) 
-            self.message = "Insufficient balance for withdraw :/"
-        elsif (amount > self.limitForDay)
-            self.message = "Maximum service limit is R$ 700"
-        else
-            self.balance -= amount
-        end
-    end
-end
+require_relative '../app/bank'
 
 describe CheckingAccount do
     describe 'Withdraw' do
