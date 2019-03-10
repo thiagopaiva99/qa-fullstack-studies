1f0688d36da0bdc9c360d9f084e9e7d121b14472
Thiago Galvani <thiagopaiva99@gmail.com>
2019-03-10 18:41:35 -0300

    Removing sleep to find element and puting a time to wait

--- meeting-capybara/spec/upload_spec.rb
+++ meeting-capybara/spec/upload_spec.rb
@@ -1,6 +1,7 @@
 describe 'Upload' do 
     before(:each) do
         visit 'http://training-wheels-protocol.herokuapp.com/upload'
+
         @file = Dir.pwd + '/spec/fixtures/file.txt'
         @image = Dir.pwd + '/spec/fixtures/capybara.jpg'
     end
@@ -17,8 +18,6 @@ describe 'Upload' do
         attach_file('file-upload', @image)
 
         click_button 'Upload'
-
-        sleep 5
 
         expect(find('#uploaded-file').text).to include 'capybara.jpg'
         expect(find('#new-image')[:src]).to include '/uploads/capybara.jpg'
