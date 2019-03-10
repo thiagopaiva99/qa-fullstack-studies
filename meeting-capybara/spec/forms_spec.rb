dd45b835ca711e0c176d7054e68e4923eeeb46df
Thiago Galvani <thiagopaiva99@gmail.com>
2019-03-10 17:50:15 -0300

    Testing forms and login flow

---
+++ meeting-capybara/spec/forms_spec.rb
@@ -0,0 +1,34 @@
+describe 'Forms' do
+    it 'should login' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'username', with: 'stark'
+        fill_in 'password', with: 'jarvis!'
+
+        click_button 'Login'
+
+        expect(find('#flash').visible?).to be true
+    end
+
+    it 'should show wrong user message' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'username', with: 'starkkk'
+        fill_in 'password', with: 'jarvis!'
+
+        click_button 'Login'
+
+        expect(find('.flash.error').visible?).to be true
+    end
+
+    it 'should show wrong password message' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'username', with: 'stark'
+        fill_in 'password', with: 'jarvis'
+
+        click_button 'Login'
+
+        expect(find('.flash.error').visible?).to be true
+    end
+end
