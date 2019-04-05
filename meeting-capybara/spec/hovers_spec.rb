6bc1cf1fb94a5392c6a8a967f51d1c176c429ef7
Thiago Galvani <thiagopaiva99@gmail.com>
2019-04-05 18:32:39 -0300

    Working with mouse hover events

---
+++ meeting-capybara/spec/hovers_spec.rb
@@ -0,0 +1,25 @@
+describe 'Mouse Hovers' do 
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/hovers'
+    end
+
+    it 'should the mouse hover on Blade' do
+        card = find('img[alt=Blade]')
+
+        card.hover
+
+        expect(page).to have_content 'Nome: Blade'
+    end
+
+    it 'should the mouse hover on Black Panter' do
+        card = find('img[alt="Pantera Negra"]')
+
+        card.hover
+
+        expect(page).to have_content 'Nome: Pantera Negra'
+    end
+
+    after(:each) do
+        sleep(3)
+    end
+end
