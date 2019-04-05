e3b3345c0cde39d0cfd9d4fb00a231e2eed5926e
Thiago Galvani <thiagopaiva99@gmail.com>
2019-04-05 18:52:08 -0300

    Working with drag and drop

---
+++ meeting-capybara/spec/drag_and_drop_spec.rb
@@ -0,0 +1,18 @@
+describe 'Drag and Drop' do 
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/drag_and_drop'
+    end
+
+    it 'should spiderman belongs to the iron man team' do
+        iron_man = find('.team-stark .column')
+        spider_man = find('img[alt$=Aranha]')
+
+        spider_man.drag_to iron_man
+
+        expect(iron_man).to have_css 'img[alt$=Aranha]'
+    end
+
+    after(:each) do
+        sleep(3)
+    end
+end
