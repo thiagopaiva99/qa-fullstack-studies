6eb9bfbcbaa12db3252d973fefa4ed98eca0e4cf
thiagopaiva99 <thiagopaiva99@gmail.com>
2020-03-22 15:59:01 -0300

    Finishing the correct scenario to test if it is authenticated

--- cucumber-web-ninja/features/step_definitions/login_steps.rb
+++ cucumber-web-ninja/features/step_definitions/login_steps.rb
@@ -1,11 +1,19 @@
-Quando("eu faço login com {string} e {string}") do |string, string2|
+Quando("eu faço login com {string} e {string}") do |email, password|
     visit "/"
 
-    sleep 3
+    find('#emailId').set email
+    find('#passId').set password
+
+    click_button 'Entrar'
+
+    sleep 1
 end
 
 Entao("devo ser autenticado") do
-    pending # Write code here that turns the phrase above into concrete actions
+    token_js_script = 'return window.localStorage.getItem("default_auth_token");'
+    token = page.execute_script(token_js_script)
+    
+    expect(token.length).to be 147
 end
 
 Entao("devo ver {string} na área logada") do |string|
