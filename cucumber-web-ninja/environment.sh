1ab63e1d07f0f4bd58a0fe7b658c4397c8ee41de
thiagopaiva99 <thiagopaiva99@gmail.com>
2020-03-22 00:41:25 -0300

    Adding the shell script to create the docker environment for the app

---
+++ cucumber-web-ninja/environment.sh
@@ -0,0 +1,8 @@
+# Creating network for all the environment
+docker network create --driver bridge skynet
+
+# Creating the container for Postgres database
+docker run --name pgdb --network=skynet -e "POSTGRES_PASSWORD=qaninja" -p 5432:5432 -v var/lib/postgresql/data -d postgres
+
+# Creating the container to run PgAdmin
+docker run --name pgadmin --network=skynet -e "PGADMIN_DEFAULT_EMAIL=root@qaninja.io" -e "PGADMIN_DEFAULT_PASSWORD=qaninja" -p 15432:80 -d dpage/pgadmin4
